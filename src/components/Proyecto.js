0023d5252f3b5e3b1301eee881f4472cb886d56a
Eamll <alexis.0804yes@gmail.com>
2023-01-26 21:45:42 -0400

    Changes in order to not have warnings

--- src/components/Proyecto.js
+++ src/components/Proyecto.js
@@ -4,22 +4,23 @@ import { trabajos } from './data/trabajos';
 export const Proyecto = () => {
     const [proyecto, setProyecto] = useState("");
 
+
     const params = useParams();
     useEffect(() => {
         let proyecto = trabajos.filter(trabajo => trabajo.id === params.id);
         setProyecto(proyecto[0]);
 
-    }, [])
+    }, [params.id])
     return (
         <div className='page page-work'>
 
             <div className='mask'>
-                <img src={"/images/" + proyecto.id + ".png"} />
+                <img src={"/images/" + proyecto.id + ".png"} alt="No se encontro el proyecto" />
             </div>
             <h1 className='heading'>{proyecto.nombre}</h1>
             <p>{proyecto.tecnologias}</p>
             <p>{proyecto.descripcion}</p>
-            <a href={"https://" + proyecto.url} target="_blank">Ir al proyecto</a>
+            <a href={"https://" + proyecto.url} target="_blank" rel='noopener noreferrer'>Ir al proyecto</a>
         </div>
     )
 }
