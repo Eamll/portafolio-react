568dd17feb74ff8148b37db9eeee13326bd93e40
Eamll <alexis.0804yes@gmail.com>
2023-01-26 20:44:14 -0400

    Changed unused dependency

--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter, NavLink, Navigate } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import { Contacto } from '../components/Contacto'
 import { Curriculum } from '../components/Curriculum'
 import { Inicio } from '../components/Inicio'
