6795f7179ec8eec8d3c0dd2ec526c34cf271fe58
Eamll <alexis.0804yes@gmail.com>
2023-04-08 17:59:57 -0400

    Filled with content two pages and added a little bit of style

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -5,9 +5,75 @@ export const Curriculum = () => {
         <div className='page'>
             <h1 className='heading'>Curriculum</h1>
             <h2>Experiencia</h2>
-            <h2>Conocmientos</h2>
-            <h2>Proyectos personales</h2>
+            <p>
+                Empresa: Consultora Solución<br />
+                Cargo: Desarrollador de Tecnología
+            </p>
+            <p>Responsabilidades:</p>
+            <ul>
+                <li>Colaboré activamente en el área de tecnología, desarrollando soluciones innovadoras para optimizar y mejorar los procesos de trabajo.</li>
+                <li>Implementé y promoví la adopción de nuevas herramientas tecnológicas que contribuyeron a una mayor organización y eficiencia dentro de la empresa.</li>
+            </ul>
+            <h2>Conocimientos</h2>
+            <div class="skills-container">
+                <div>
+                    <h4>MERN Stack</h4>
+                    <ul>
+                        <li>MongoDB</li>
+                        <li>Express.js</li>
+                        <li>React.js</li>
+                        <li>Node.js</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Bases de datos</h4>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                        <li>Firebase</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Otros conocimientos Back End</h4>
+                    <ul>
+                        <li>C#</li>
+                        <li>Sequelize (ORM)</li>
+                        <li>Express JWT</li>
+                        <li>JSON Web Token</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Front End</h4>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Desarrollo móvil multiplataforma</h4>
+                    <ul>
+                        <li>Flutter-Dart</li>
+                    </ul>
+                </div>
+            </div>
+
             <h2>Voluntariados</h2>
+            <ul>
+                <li>
+                    <h4>2018-2019/UTEPSA University</h4>
+                    <p>The program "CURSO DE COMPUTACIÓN ADULTO MAYOR" teaching elderly people the basics of computers in order for them to adapt to our modern times.</p>
+                </li>
+                <li>
+                    <h4>2021-Current Time/UTEPSA University</h4>
+                    <p>The program "EduActivistas" with the support of Switzerland Embassy in order to reduce the breaches between men and women.</p>
+                </li>
+
+            </ul>
+            {/* <h4>2018-2019/UTEPSA University</h4>
+            <p>The program "CURSO DE COMPUTACIÓN ADULTO MAYOR" teaching elderly people the basics of computers in order for them to adapt to our modern times.</p>
+            <h4>2021-Current Time/UTEPSA University</h4>
+            <p>The program "EduActivistas" with the support of Switzerland Embassy in order to reduce the breaches between men and women.</p> */}
             <ul>
 
             </ul>
--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -6,12 +6,24 @@ export const Servicios = () => {
             <h1 className='heading'>Servicios</h1>
             <section className='services'>
                 <article className='service'>
-                    <h2>Desarrollo Web</h2>
-                    <p>Creo tu página web desde cero</p>
+                    <h2>Desarrollo Web Full Stack</h2>
+                    <p>Desarrollo de aplicaciones web completas utilizando el MERN Stack y otras tecnologías.</p>
+                </article>
+                <article className='service'>
+                    <h2>Desarrollo de API</h2>
+                    <p>Creación y diseño de APIs RESTful para conectar tu aplicación a bases de datos y otros servicios.</p>
+                </article>
+                <article className='service'>
+                    <h2>Desarrollo de aplicaciones móviles</h2>
+                    <p>Desarrollo de aplicaciones móviles multiplataforma utilizando Flutter y Dart.</p>
                 </article>
                 <article className='service'>
                     <h2>Diseño Web</h2>
-                    <p>Hago que tu web se vea bonita</p>
+                    <p>Diseño de sitios web atractivos y funcionales utilizando HTML, CSS y JavaScript.</p>
+                </article>
+                <article className='service'>
+                    <h2>Integración de bases de datos</h2>
+                    <p>Integración de bases de datos como MySQL, PostgreSQL y Firebase en tus proyectos web y móviles.</p>
                 </article>
             </section>
         </div>
