4acf78bb380e37997bfc90159440aa5c2c4d12da
Eamll <alexis.0804yes@gmail.com>
2023-04-12 22:44:39 -0400

    .

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,34 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-import { NetlifyForm, Honeypot } from 'react-netlify-forms'
+export const Contacto = () => {
 
-export const Contacto = () => (
-    <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
-        {({ handleChange, success, error }) => (
-            <>
-                <Honeypot />
-                {success && <p>Thanks for contacting us!</p>}
-                {error && (
-                    <p>Sorry, we could not reach our servers. Please try again later.</p>
-                )}
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        try {
+            const response = await fetch("/contacto", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams(formData).toString(),
+            });
+
+            if (!response.ok) {
+                throw new Error("Form submission failed");
+            }
+
+            alert("Form submitted successfully!");
+        } catch (error) {
+            console.error(error);
+            alert("There was an error submitting the form. Please try again.");
+        }
+    };
+    return (
+        <div>
+            <h4>Contacto</h4>
+            <form className="contact" method="POST" data-netlify="true" action="/" onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' id='name' onChange={handleChange} />
+                    <label htmlFor="name-form">Nombre</label>
+                    <input type="text" name="name" required id="name-form" />
                 </div>
                 <div>
-                    <label htmlFor='message'>Message:</label>
-                    <textarea
-                        type='text'
-                        name='message'
-                        id='message'
-                        rows='4'
-                        onChange={handleChange}
-                    />
+                    <label htmlFor="lastname-form">Apellidos</label>
+                    <input type="text" name="apellidos" id="lastname-form" />
                 </div>
                 <div>
-                    <button type='submit'>Submit</button>
+                    <label htmlFor="email-form">Email</label><br></br>
+                    <input type="email" name="email" required id="email-form" />
                 </div>
-            </>
-        )}
-    </NetlifyForm>
-)
+
+                <div>
+                    <label htmlFor="message-form">Motivo de contacto</label>
+                    <textarea name="message" rows="7" id="message-form"></textarea>
+                </div>
+                <div>
+                    <div data-netlify-recaptcha="true"></div>
+                </div>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    )
+}
