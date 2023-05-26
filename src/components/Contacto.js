ef8e3bf52f129230b59ae5fd1f9a9af47a213dad
Eamll <alexis.0804yes@gmail.com>
2023-05-26 09:40:07 -0400

    .

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,48 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
-
 
 export const Contacto = () => {
     const formEndpoint = 'https://getform.io/f/23e33eb5-d5fd-448c-8141-f4572037a747';
     const [submitting, setSubmitting] = useState(false);
-    // const navigate = useNavigate();
 
-    useEffect(() => {
-        if (submitting) {
-            (async () => {
-                const formData = new FormData();
-                const data = new URLSearchParams([...formData.entries()]);
-                try {
-                    const response = await axios.post(formEndpoint, data, {
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    });
-
-                    if (response.status === 200) {
-                        console.log('Form submitted successfully');
-                        // navigate('/thank-you'); // Redirect to the thank you page
-                    } else {
-                        console.error('Form submission failed');
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-
-                setSubmitting(false);
-
-            })();
-        }
-    }, [submitting]);
-
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         setSubmitting(true);
+
+        const form = event.target;
+        const formData = new FormData(form);
+        const data = new URLSearchParams([...formData.entries()]);
+
+        try {
+            const response = await axios.post(formEndpoint, data, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
+
+            if (response.status === 200) {
+                console.log('Form submitted successfully');
+            } else {
+                console.error('Form submission failed');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        setSubmitting(false);
     };
+
     return (
         <div>
-
             <h4>Contacto</h4>
             <form onSubmit={handleSubmit} name='contact'>
                 <input type='hidden' name='form-name' value='contact' />
@@ -57,14 +48,16 @@ export const Contacto = () => {
                 </div>
                 <div>
                     <label htmlFor='email-form'>Email</label>
-                    <br></br>
+                    <br />
                     <input type='email' name='email' required id='email-form' />
                 </div>
                 <div>
                     <label htmlFor='message-form'>Motivo de contacto</label>
                     <textarea name='message' rows='7' id='message-form'></textarea>
                 </div>
-                <button type='submit'>Enviar</button>
+                <button type='submit' disabled={submitting}>
+                    {submitting ? 'Enviando...' : 'Enviar'}
+                </button>
             </form>
         </div>
     );
